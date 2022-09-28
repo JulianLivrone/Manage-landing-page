0feeff295b3d1487ca6a34178b391e5c85f49d10
JulianLivrone <julian.livrone@gmail.com>
2022-09-28 18:55:28 -0300

    Starting the navigation

---
+++ main.js
@@ -0,0 +1,9 @@
+const navToggle = document.querySelector('.mobile-nav-toggle');
+const primaryNav = document.querySelector('.primary-navigation');
+
+navToggle.addEventListener('click', () => {
+  primaryNav.hasAttribute('data-visible')
+    ? navToggle.setAttribute('area-expanded', false)
+    : navToggle.setAttribute('area-expanded', true);
+  primaryNav.toggleAttribute('data-visible');
+});
